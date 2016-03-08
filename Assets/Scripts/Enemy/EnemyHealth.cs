b59138271c10364e1b55244b2d7eab34ad17e959
Rafael Nabeth <rafaelnabeth@gmail.com>
2016-03-08 22:27:21 +0000

    Adding Assets

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public int startingHealth = 100;
+	public int currentHealth;
+	public float sinkSpeed = 2.5f;
+	public int scoreValue = 10;
+	public AudioClip deathClip;
+
+	Animator anim;
+	AudioSource enemyAudio;
+	ParticleSystem hitParticles;
+	CapsuleCollider capsuleCollider;
+	bool isDead;
+	bool isSinking;
+
+	void Awake ()
+	{
+		anim = GetComponent <Animator> ();
+		enemyAudio = GetComponent <AudioSource> ();
+		hitParticles = GetComponentInChildren <ParticleSystem> ();
+		capsuleCollider = GetComponent <CapsuleCollider> ();
+
+		currentHealth = startingHealth;
+	}
+
+	void Update ()
+	{
+		if (isSinking) {
+			transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+		}
+	}
+
+	public void TakeDamage (int amount, Vector3 hitPoint)
+	{
+		if (isDead) {
+			return;
+		}
+
+		enemyAudio.Play ();
+
+		currentHealth -= amount;
+            
+		hitParticles.transform.position = hitPoint;
+		hitParticles.Play ();
+
+		if (currentHealth <= 0) {
+			Death ();
+		}
+	}
+
+	void Death ()
+	{
+		isDead = true;
+
+		//You don't actually hit triggers, so the object is not an obstacle anymore
+		capsuleCollider.isTrigger = true;
+
+		anim.SetTrigger ("Dead");
+
+		ScoreManager.score += scoreValue;
+		
+		enemyAudio.clip = deathClip;
+		enemyAudio.Play ();
+	}
+
+	public void StartSinking ()
+	{
+		GetComponent <NavMeshAgent> ().enabled = false;
+		GetComponent <Rigidbody> ().isKinematic = true; //When moving a collider in the scene Unity recalculates the static geometry if object is not kinematic
+		isSinking = true;
+		Destroy (gameObject, 2f); //Finished sinking after 2 seconds so we can destroy it
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	//It gets called regardless if the script is enabled or not
+	void Awake ()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	//Called when there are any physics update
+	void FixedUpdate ()
+	{
+		//GetAxisRaw: Only have -1, 0, 1 - no variation between - instead of slowing accelerate it snaps into full speed
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+		Animating (h, v);
+	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning ()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	void Animating (float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking);
+	}
+}
